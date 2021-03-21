b74d22bbd0ef4415010299af200f04db57064f69
kaledinandrew <kaledin.andr@gmail.com>
2021-03-21 17:48:53 +0300

    Project is initialized

---
+++ src/main/java/com/project/bigbrother/BigBrotherApplication.java
@@ -0,0 +1,13 @@
+package com.project.bigbrother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigBrotherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigBrotherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/bigbrother/BigBrotherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bigbrother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigBrotherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
