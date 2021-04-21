00c6a15934d9ec2a3c6117bb51b01524fd2b950e
kaledinandrew <kaledin.andr@gmail.com>
2021-04-21 17:01:46 +0300

    Moved to postgresql and connected to heroku database

--- src/main/java/rest/AdminController.java
+++ src/main/java/rest/AdminController.java
@@ -5,10 +5,7 @@ import models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import repositories.UserRepository;
 import service.UserService;
 
@@ -31,15 +28,15 @@ public class AdminController {
         return "hello, admin";
     }
 
-    @GetMapping(value = "users")
+    @GetMapping(value = "all_users")
     public ResponseEntity<List<UserDto>> getAllUsers() {
         return new ResponseEntity<>(
                 userRepository.findAll().stream().map(UserDto::fromUser).collect(Collectors.toList()),
                 HttpStatus.OK);
     }
 
-    @GetMapping(value = "users/{id}")
-    public ResponseEntity<UserDto> getUserById(@PathVariable(name = "id") Long id) {
+    @GetMapping(value = "user")
+    public ResponseEntity<UserDto> getUserById(@RequestParam(name = "id") Long id) {
         User user = userRepository.findById(id).orElse(null);
         if (user == null) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
